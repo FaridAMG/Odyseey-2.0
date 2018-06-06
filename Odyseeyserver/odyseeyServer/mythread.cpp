8c2b7d5c7cd56dcb27c3741c103d6278ea7d3b55
Unknown <faridmarin25@gmail.com>
2018-06-06 01:46:38 -0600

    Server Created!

---
+++ Odyseeyserver/odyseeyServer/mythread.cpp
@@ -0,0 +1,49 @@
+#include "mythread.h"
+
+MyThread::MyThread(int ID,QObject *parent) : QThread(parent)
+{
+
+    this->socketDescriptor = ID;
+
+}
+
+void MyThread::run(){
+    // Thread starts here
+
+    qDebug()<< socketDescriptor << "Starting thread";
+    socket = new QTcpSocket();
+    if(!socket->setSocketDescriptor(this->socketDescriptor)){
+
+        emit error(socket->error());
+        return;
+
+    }
+
+    connect(socket,SIGNAL(readyRead()),this,SLOT(readyRead()),Qt::DirectConnection);
+    connect(socket,SIGNAL(disconnected()),this,SLOT(disconnected()),Qt::DirectConnection);
+
+    qDebug()<< socketDescriptor << "Client Connected";
+
+
+   exec(); //means this thread will stay alive forever, unill you disconnect it
+}
+
+
+void MyThread::readyRead(){
+
+    QByteArray Data = socket->readAll();
+    qDebug()<< socketDescriptor <<"Data in..:"<< Data;
+    socket->write(Data);
+
+}
+
+
+
+void MyThread::disconnected(){
+
+    qDebug()<< socketDescriptor <<"Disconnected..:";
+
+    socket->deleteLater();
+    exit(0);
+
+}
